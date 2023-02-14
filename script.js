892fa17d058a8e65a649c875a1b64293e8ffb840
covafest <31441501+covafest@users.noreply.github.com>
2023-02-14 18:04:59 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-  $(document).ready(function() {
+$(document).ready(function(){
 
-    $('#nav-item-2').on('click', muestraMenu);
+  /*DESCENDER A MAS INFO*/
+  $(".masinfo").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $('#article-wrap').offset().top
+    }, 2000);});
 
+  /*SUBIR A TOP DE LA PAGINA*/
+  $(".arriba").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: 0 }, 2000);});
+
+/*IR A LA PAGINA ANTERIOR*/
+
+
+      $('.volver').click(function() {
+      window.history.back();
+      return false;
+      });
+
+  
+
+  /*ANIMACION ICONO MENU*/
+  $('#menu-icono').click(function(){
+    $(this).toggleClass('open');
   });
 
+  /*DESPLEGAR MENU*/
   $(document).ready(function() {
-
-    $('.cerrar').on('click', ocultaMenu);
-
+    $('#nav-item-2').on('click', muestraMenu);
   });
 
   function muestraMenu (){
-    $('#menu-desplegado').toggleClass('visible');
+  $('#menu-desplegado').toggleClass('visible');
+  };
+    
+  /*CTA CONFIRMACION IR A FORMULARIO*/
+  $('#CTA-wrap').on('click', alFormulario);
+
+  function alFormulario (){
+    window.open('confirmacion.html');
   };
 
-  function ocultaMenu (){
-    $('#menu-desplegado').toggleClass('visible');
-  };
-
-  $(document).ready(function(){
-	$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function(){
-		$(this).toggleClass('open');
-	  });
   });
+
