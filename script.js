12c3ca49a87520c7785e181dbc6849b9d4f59e89
covafest <31441501+covafest@users.noreply.github.com>
2023-02-15 18:08:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -11,16 +11,12 @@ $(document).ready(function(){
     $([document.documentElement, document.body]).animate({
         scrollTop: 0 }, 2000);});
 
-/*IR A LA PAGINA ANTERIOR*/
-
-
-      $('.volver').click(function() {
-      window.history.back();
-      return false;
-      });
-
+  /*IR A LA PAGINA ANTERIOR*/
+  $('.volver').click(function() {
+    window.history.back();
+    return false;
+  });
   
-
   /*ANIMACION ICONO MENU*/
   $('#menu-icono').click(function(){
     $(this).toggleClass('open');
@@ -42,5 +38,29 @@ $(document).ready(function(){
     window.open('confirmacion.html');
   };
 
+    $('.accordion-list > li > .answer').hide();
+      
+    $('.accordion-list > li').click(function() {
+      if ($(this).hasClass("active")) {
+        $(this).removeClass("active").find(".answer").slideUp();
+        $("#icono-pregunta", this).removeClass("rotar");
+
+        
+
+      } else {
+        $(".accordion-list > li.active .answer").slideUp();
+        $(".accordion-list > li.active > img").removeClass("rotar");
+        $(".accordion-list > li.active").removeClass("active");
+        $("#icono-pregunta", this).addClass("rotar");
+        $(this).addClass("active").find(".answer").slideDown();
+      }
+      return false;
+    });
+
   });
+
+  $(".class").click(function(){
+    $(".subclass", this).css("visibility","visible");
+});
+
 
